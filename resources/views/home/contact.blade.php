bf2c3009124b4f6ff7c756798aa6d718db00509b
Angela Franklin <afranklin@diesellaptops.com>
2022-04-18 08:45:58 -0400

    Added my project

---
+++ resources/views/home/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Contact Page')
+
+@section('content')
+    <h1>Contacts Page</h1>
+@endsection
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'Blog Posts')
+
+@section('content')
+    @forelse ($posts as $key => $post )
+        {{-- @break($key == 2) --}}
+        @if ($loop->even)
+            <div>{{ $key }}.{{ $post['title'] }}</div>
+        @else
+            <div style="background-color: silver">{{ $key }}.{{ $post['title'] }}</div>
+        @endif
+        @empty
+        No posts found!
+    @endforelse
+@endsection
