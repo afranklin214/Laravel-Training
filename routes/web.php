18f2255b297817f7a06dd84d7c979d41d5e0783a
Angela Franklin <afranklin@diesellaptops.com>
2022-04-18 13:34:50 -0400

    completed 11-47

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -27,6 +28,7 @@ $posts = [
         'is_new' => true,
         'has_comments' => true
     ],
+
     2 => [
         'title' => 'Intro to PHP',
         'content' => 'This is a short intro to PHP',
@@ -35,21 +37,23 @@ $posts = [
     ]
 ];
 
-Route::get('/posts', function() use($posts) {
-    // dd(request()->all());
-    dd((int)request()->query('page', 1));
-   return view('posts.index', ['posts' => $posts]); 
-});
+Route::resource('posts', PostsController::class)->only(['index', 'show']);
 
-Route::get('/posts/{id}', function ($id) use ($posts) {
-    abort_if(!isset($posts[$id]), 404);
+// Route::get('/posts', function() use($posts) {
+//     // dd(request()->all());
+//     dd((int)request()->query('page', 1));
+//    return view('posts.index', ['posts' => $posts]); 
+// });
 
-    return view('posts.show', ['post' => $posts[$id]]);
-})
-// ->where([
-//     'id' => '[0-9]+'
-// ])
-->name('posts.show');
+// Route::get('/posts/{id}', function ($id) use ($posts) {
+//     abort_if(!isset($posts[$id]), 404);
+
+//     return view('posts.show', ['post' => $posts[$id]]);
+// })
+// // ->where([
+// //     'id' => '[0-9]+'
+// // ])
+// ->name('posts.show');
 
 Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
     return 'Posts from ' . $daysAgo . ' days ago';
