01e766bcc801d8d76ce3d191f9c4007cab93450d
Angela Franklin <afranklin@diesellaptops.com>
2022-04-18 10:29:26 -0400

    Completed section 10 number 36 and 37

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,13 @@ Route::get('/posts/{id}', function ($id) use ($posts) {
 Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
     return 'Posts from ' . $daysAgo . ' days ago';
 })->name('posts.recent.index');
+
+Route::get('/fun/responses', function() use($posts) {
+    return response($posts, 201)
+    ->header('Content-Type', 'applicaion/json')
+    ->cookie('MY_COOKIE', 'Angela Franklin', 3600);
+});
+
+Route::get('fun/redirect', function() {
+    return redirect('/contact');
+});
