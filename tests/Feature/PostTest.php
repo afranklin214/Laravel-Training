9b679122793c48107886995d12277055964bfeae
Angela Franklin <afranklin@diesellaptops.com>
2022-04-22 09:44:39 -0400

    completed 18-87

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\BlogPost;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testNoBlogPostsWhenDBIsEmpty()
+    {
+        $response = $this->get('/posts');
+
+        $response->assertSeeText('No posts found!');
+    }
+
+    public function testSee1BlogPostWhenThereIs1()
+    {
+        $post = new BlogPost();
+        $post->title = 'New title';
+        $post->content = 'Content of the blog post';
+        $post->save();
+
+        $response = $this->get('/posts');
+
+        $response->assertSeeText('New title');
+    }
+}
