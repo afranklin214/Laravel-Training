1fa26d740c89d6e48be574689536473b85616897
Angela Franklin <103783475+afranklin214@users.noreply.github.com>
2022-05-02 16:27:50 -0400

    trying to create login form

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Route::get('/', [HomeController::class, 'home'])->name('home.index');
 Route::get('/contact', [HomeController::class, 'contact'])->name('home.contact');
 Route::get('/single', AboutController::class);
 
+Auth::routes();
+
 $posts = [
     1 => [
         'title' => 'Intro to Laravel',
