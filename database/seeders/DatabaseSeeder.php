fe15d321847f76a1be7db797c2a51fc10aab9664
Angela Franklin <103783475+afranklin214@users.noreply.github.com>
2022-05-10 11:58:23 -0400

    completed sect 24-123

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,7 +13,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        \App\Models\User::factory(20)->create();
-        \App\Models\User::factory()->john_doe()->create();
+        $else = \App\Models\User::factory(20)->create();
+        $doe = \App\Models\User::factory()->john_doe()->create();
+
+        $users = $else->concat([$doe]);
+
+        $posts = \App\Models\BlogPost::factory(50)->make()->each(function($post) use ($users) {
+            $post->user_id = $users->random()->id;
+            $post->save();
+        });
+
+        $comments = \App\Models\Comment::factory(150)->make()->each(function($comment) use ($posts) {
+            $comment->blog_post_id = $posts->random()->id;
+            $comment->save();
+        });
     }
 }
