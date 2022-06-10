fca3f279f6dc2be56f7a765f1f6e95c4241978d1
Angela Franklin <103783475+afranklin214@users.noreply.github.com>
2022-06-10 10:41:30 -0400

    completed 27:148

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -38,15 +38,13 @@
                         </ul>
                     </div>
                 </div>
-            </div>
-
-            <div class="row mt-4">
-                <div class="col-4">
-                    <div class="card" style="width: 20rem;">
+            
+                <div class="row mt-4">
+                    <div class="card" style="width: 100%;">
                         <div class="card-body">
                             <h5 class="card-title">Most Active</h5>
                             <h6 class="card-subtitle mb-2 text-muted">
-                                Users with most posts written
+                                    Users with most posts written
                             </h6>
                         </div>
                         <ul class="list-group list-group-flush">
@@ -54,7 +52,25 @@
                                 <li class="list-group-item">
                                     {{  $user->name }}
                                 </li>
-                            @endforeach
+                                @endforeach
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="row mt-4">
+                    <div class="card" style="width: 100%;">
+                        <div class="card-body">
+                            <h5 class="card-title">Most Active Last Month</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">
+                                    Users with most posts written in the last month
+                            </h6>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            @foreach ($mostActiveLastMonth as $user)
+                                <li class="list-group-item">
+                                    {{  $user->name }}
+                                </li>
+                                @endforeach
                         </ul>
                     </div>
                 </div>
