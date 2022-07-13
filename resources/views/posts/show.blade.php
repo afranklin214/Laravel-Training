a8691c35e8a122acb550f1e2f164e0c018b592ce
Angela Franklin <103783475+afranklin214@users.noreply.github.com>
2022-07-13 11:16:56 -0400

    completed 30-174

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,27 +3,34 @@
 @section('title', $post->title)
 
 @section('content')
-    <h1> {{ $post->title }}</h1>
-    @if ((new Carbon\Carbon())->diffInMinutes($post->created_at) < 20)
-        <span>
-            Brand New Post!
-        </span>
-    @endif
-    <p>{{ $post->content }}</p>
-    <p>Added {{ $post->created_at->diffForHumans() }}</p>
+<div class="row">
+    <div class="col-8">
+        <h1> {{ $post->title }}</h1>
+        @if ((new Carbon\Carbon())->diffInMinutes($post->created_at) < 20)
+            <span>
+                Brand New Post!
+            </span>
+        @endif
+        <p>{{ $post->content }}</p>
+        <p>Added {{ $post->created_at->diffForHumans() }}</p>
 
-    {{-- <x-updated :date="$post->created_at" :userId="$post->user->id" :name="$post->user->name" /> --}}
-    
-    @tags(['tags' => $post->tags])@endtags
-    
-    <p>Currently read by {{ $counter }} people</p>
-    <h4>Comments</h4>
+        {{-- <x-updated :date="$post->created_at" :userId="$post->user->id" :name="$post->user->name" /> --}}
+        
+        @tags(['tags' => $post->tags])@endtags
+        
+        <p>Currently read by {{ $counter }} people</p>
+        <h4>Comments</h4>
 
-    @forelse($post->comments as $comment)
-        <p>
-            {{ $comment->content }}, added {{ $comment->created_at->diffForHumans()}}
-        </p>
-    @empty
-        <p>No comments yet!</p>
-    @endforelse
+        @forelse($post->comments as $comment)
+            <p>
+                {{ $comment->content }}, added {{ $comment->created_at->diffForHumans()}}
+            </p>
+        @empty
+            <p>No comments yet!</p>
+        @endforelse
+    </div>
+    <div class="col-4">
+        @include('posts._activity')
+    </div>
+</div>
 @endsection
