74f7d08af501114eb747d1c6cde8cca7fdc61262
Angela Franklin <103783475+afranklin214@users.noreply.github.com>
2022-07-20 08:38:40 -0400

    Completed 32-182

--- resources/views/posts/partials/form.blade.php
+++ resources/views/posts/partials/form.blade.php
@@ -1,14 +1,20 @@
 <div class="form-group">
     <label for="title">Title</label>
-    <input id="title" type="text" name="title" class="form-control" value="{{ old('title', optional($post ?? null)->title) }}">
+    <input id="title" type="text" name="title" class="form-control" value="{{ old('title', optional($post ?? null)->title) }}"/>
 </div>
     @error('title')
         <div class="alert alert-danger">{{ $message }}</div>
     @enderror
-    <div class="form-group">
-        <label for="content">Content</label>
-        <textarea class="form-control" id="content" name="content">{{ old('content', optional($post ?? null)->content) }}</textarea>
-    </div>
+<div class="form-group">
+    <label for="content">Content</label>
+    <textarea class="form-control" id="content" name="content">{{ old('content', optional($post ?? null)->content) }}</textarea>
+</div>
+
+<div class="form-group">
+    <label>Thumbnail</label>
+    <input type="file" name="thumbnail" class="form-control-file"/`>
+</div>
+
    
-    <x-errors :errors="$errors->first('content')" />
+<x-errors :errors="$errors->first('content')" />
     
