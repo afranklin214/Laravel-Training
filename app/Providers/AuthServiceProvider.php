1ca58adde76c3ab02a89bfb2a2dc34cac0f77437
Angela Franklin <103783475+afranklin214@users.noreply.github.com>
2022-07-25 14:22:41 -0400

    Completed 33-192

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -16,6 +16,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         'App\Models\Model' => 'App\Policies\ModelPolicy',
         'App\BlogPost' => 'App\Policies\BlogPostPolicy',
+        'App\User' => 'App\Policies\UserPolicy'
     ];
 
     /**
