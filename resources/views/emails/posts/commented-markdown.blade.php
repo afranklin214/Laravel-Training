55e1516c7fb85d5a9f96fd7a5a422422ad1f8f80
Angela Franklin <103783475+afranklin214@users.noreply.github.com>
2022-08-10 11:24:49 -0400

    completed 36-217

--- resources/views/emails/posts/commented-markdown.blade.php
+++ resources/views/emails/posts/commented-markdown.blade.php
@@ -1,12 +1,21 @@
 @component('mail::message')
-# Introduction
+# Comment was posted on your blog post
 
-Hello from Markdown!
+Hi {{  $comment->commentable->user->name }},
 
-@component('mail::button', ['url' => ''])
-Button Text
+Someone has commented on your blog post
+
+@component('mail::button', ['url' => route('posts.show', ['post' => $comment->commentable->id ])])
+View the Blog Post
 @endcomponent
 
+@component('mail::button', ['url' => route('users.show', ['user' => $comment->user->id])])
+Visit {{ $comment->user->name }} profile
+@endcomponent
+
+@component('mail::panel')
+{{ $comment->content}}
+@endcomponent
 Thanks,<br>
 {{ config('app.name') }}
 @endcomponent
