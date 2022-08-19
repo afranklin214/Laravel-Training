8f2fe0e24baf985cb8da8285cb352f22127ff190
Angela Franklin <103783475+afranklin214@users.noreply.github.com>
2022-08-19 15:49:21 -0400

    completed 39-235

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,5 +3,14 @@
 @section('title', 'Home Page')
 
 @section('content')
-    <h1>Hello world</h1>
+    <h1>{{ __('messages.welcome') }}</h1>
+    <h1>@lang('messages.welcome')</h1>
+
+    <p>{{ __('messages.example_with_value', ['name' => 'Blog User']) }}</p>
+
+    <p>{{ trans_choice('messages.plural', 0, ['a' =>1]) }}</p>
+    <p>{{ trans_choice('messages.plural', 1, ['a' =>1]) }}</p>
+    <p>{{ trans_choice('messages.plural', 2, ['a' =>1]) }}</p>
+
+    <p>This is the content of the main page!</p>
 @endsection
