311444f7b4e1de73197eee610d5f9228fce53c2b
Angela Franklin <103783475+afranklin214@users.noreply.github.com>
2022-08-23 15:45:22 -0400

    completed 40-244

--- app/Services/Counter.php
+++ app/Services/Counter.php
@@ -2,23 +2,34 @@
 
 namespace App\Services;
 
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Contracts\Cache\Factory as Cache;
+
 
 class Counter
 {
     private $timeout;
-    public function __construct(int $timeout)
+    private $cache;
+    private $session;
+    private $supportsTags;
+
+    public function __construct(Cache $cache, Session $session, int $timeout)
     {
+        $this->cache = $cache;
         $this->timeout = $timeout;
+        $this->session = $session;
+        $this->supportsTags = method_exists($cache, 'tags');
     }
 
     public function increment(string $key, array $tags = null): int
     {
-        $sessionId = session()->getId();
+        $sessionId = $this->session->getId();
         $counterKey = "{$key}-counter";
         $usersKey = "{$key}-users";
 
-        $users = Cache::tags(['blog-post'])->get($usersKey, []);
+        $cache = $this->supportsTags && null !== $tags ? $this->cache->tags($tags) : $this->cache;
+
+        $users = $cache->get($usersKey, []);
         $usersUpdate = [];
         $difference = 0;
         $now = now();
@@ -39,15 +50,15 @@ class Counter
         }
 
         $usersUpdate[$sessionId] = $now;
-        Cache::tags(['blog-post'])->forever($usersKey, $usersUpdate);
+        $cache->forever($usersKey, $usersUpdate);
 
-        if (!Cache::tags(['blog-post'])->has($counterKey)) {
-            Cache::tags(['blog-post'])->forever($counterKey, 1);
+        if (!$cache->has($counterKey)) {
+            $cache->forever($counterKey, 1);
         } else {
-            Cache::tags(['blog-post'])->increment($counterKey, $difference);
+            $cache->increment($counterKey, $difference);
         }
         
-        $counter = Cache::tags(['blog-post'])->get($counterKey);
+        $counter = $cache->get($counterKey);
 
         return $counter;
     }
