745eb01f1ca3ae355051114d0791ad1de05b2f2a
Angela Franklin <103783475+afranklin214@users.noreply.github.com>
2022-08-24 09:22:40 -0400

    completed 41-247

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/secret', [HomeController::class, 'secret'])
 Route::get('/single', AboutController::class);
 Route::resource('posts', PostsController::class);
 Route::get('/posts/tag/{tag}', PostTagController::class . '@index')->name('posts.tags.index');
-Route::resource('posts.comments', PostCommentController::class)->only(['store']);
+Route::resource('posts.comments', PostCommentController::class)->only(['index','store']);
 Route::resource('users.comments', UserCommentController::class)->only(['store']);
 Route::resource('users', UserController::class)->only(['show', 'edit', 'update']);
 
